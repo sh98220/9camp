6f0ba6815cce8fc3ca337a05a7e0f00500d45cb3
JungHyun <JungHyun@DESKTOP-7RIIVGU>
2023-05-11 15:37:10 +0900

    asd

--- 9camp/src/main/java/com/util/DBConn.java
+++ 9camp/src/main/java/com/util/DBConn.java
@@ -2,38 +2,69 @@ package com.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConn {
 	private static Connection conn = null;
-	
+
 	private DBConn() {
-		
 	}
 	
 	public static Connection getConnection() {
-		String url = "jdbc:oracle:thin:@//211.238.142.72/XE";
-		String user = "goocamp";
+		// String url = "jdbc:oracle:thin:@127.0.0.1:1521:ORCL";  // 11g
+		String url = "jdbc:oracle:thin:@//127.0.0.1:1521/XE"; // 12C 이상
+		String user = "bluesky";
 		String pwd = "java$!";
 		
 		if(conn == null) {
 			try {
 				Class.forName("oracle.jdbc.driver.OracleDriver");
 				conn = DriverManager.getConnection(url, user, pwd);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
+			}catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
 		
+		return conn;
+	}
+
+	public static Connection getConnection(String url, String user, String pwd) {
+		if(conn == null) {
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn = DriverManager.getConnection(url, user, pwd);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return conn;
+	}
+
+	public static Connection getConnection(String url, String user, String pwd, String internal_logon) {
+		if(conn == null) {
+			try {
+				Properties info = new Properties();
+				info.put("user", user);
+				info.put("password", pwd);
+				info.put("internal_logon", internal_logon);  // sysdba, Normal 등 roll
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn = DriverManager.getConnection(url, info);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
 		return conn;
 	}
 	
 	public static void close() {
+		if(conn == null) {
+			return;
+		}
+		
 		try {
-			if(conn != null && ! conn.isClosed()) {
+			if(! conn.isClosed()) {
 				conn.close();
 			}
 		} catch (Exception e) {
