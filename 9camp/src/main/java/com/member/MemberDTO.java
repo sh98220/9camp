cd9dae3000f2c83e8bf804af79b2ae9772a663d3
Jina <Jina@DESKTOP-SOIQ81S>
2023-05-12 16:34:19 +0900

    Merge branch 'main' of https://github.com/sh98220/9camp.git into main

---
+++ 9camp/src/main/java/com/member/MemberDTO.java
@@ -0,0 +1,77 @@
+package com.member;
+
+public class MemberDTO {
+	private String userId;
+	private String userName;
+	private String userPwd;
+	private String userTel;
+	private String userBirth;
+	private String userNickName;
+	private String userEmail;
+	private String userRegDate;
+	private long userPoint;
+	private String userUpdateDate;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getUserTel() {
+		return userTel;
+	}
+	public void setUserTel(String userTel) {
+		this.userTel = userTel;
+	}
+	public String getUserBirth() {
+		return userBirth;
+	}
+	public void setUserBirth(String userBirth) {
+		this.userBirth = userBirth;
+	}
+	public String getUserNickName() {
+		return userNickName;
+	}
+	public void setUserNickName(String userNickName) {
+		this.userNickName = userNickName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserRegDate() {
+		return userRegDate;
+	}
+	public void setUserRegDate(String userRegDate) {
+		this.userRegDate = userRegDate;
+	}
+	public long getUserPoint() {
+		return userPoint;
+	}
+	public void setUserPoint(long userPoint) {
+		this.userPoint = userPoint;
+	}
+	public String getUserUpdateDate() {
+		return userUpdateDate;
+	}
+	public void setUserUpdateDate(String userUpdateDate) {
+		this.userUpdateDate = userUpdateDate;
+	}
+	
+	
+
+}
