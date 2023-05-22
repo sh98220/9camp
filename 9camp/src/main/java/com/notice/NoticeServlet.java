946648d0f61540d6b0fe962ace9e0503681730e6
user <user@DESKTOP-0L11UB0>
2023-05-22 17:26:50 +0900

    민찬 - notice DTO, DAO 작성

---
+++ 9camp/src/main/java/com/notice/NoticeServlet.java
@@ -0,0 +1,57 @@
+package com.notice;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.member.SessionInfo;
+import com.util.MyUploadServlet;
+
+@MultipartConfig
+@WebServlet("/notice/*")
+public class NoticeServlet extends MyUploadServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private String pathname;
+
+	@Override
+	protected void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		
+		String uri = req.getRequestURI();
+		String cp = req.getContextPath();
+		
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		
+		String root = session.getServletContext().getRealPath("/");
+		pathname = root + "uploads" + File.separator + "notice";
+		
+		/*
+		if(uri.indexOf("list.do") != -1) {
+			list(req, resp);
+		} else if(uri.indexOf("write.do") != -1) {
+			writeForm(req, resp);
+		} else if(uri.indexOf("write_ok.do") != -1) {
+			writeSubmit(req, resp);
+		} else if(uri.indexOf("article.do") != -1) {
+			article(req, resp);
+		} else if(uri.indexOf("update.do") != -1) {
+			updateForm(req, resp);
+		} else if(uri.indexOf("update_ok.do") != -1) {
+			updateSubmit(req, resp);
+		} else if(uri.indexOf("deleteFile.do") != -1) {
+			deleteFile(req, resp);
+		} else if(uri.indexOf("delete.do") != -1) {
+			delete(req, resp);
+		} 
+		*/
+	}
+
+}
